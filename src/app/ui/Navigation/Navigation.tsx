263d3cd3c6b849be03cc7db3495d1e1238876982
Alexander Nemtsev <mrcrazy696@gmail.com>
2025-06-02 19:55:16 +0300

    вынесены роуты

--- src/app/ui/Navigation/Navigation.tsx
+++ src/app/ui/Navigation/Navigation.tsx
@@ -1,22 +1,14 @@
 import { TabNav } from '@radix-ui/themes';
-import { Link, useLocation } from 'wouter';
 
-import { link } from './Navigation.css';
+import { NavLink } from './NavLink';
+import { routes } from '../../routes';
 
-export const Navigation = () => {
-  const [location] = useLocation();
-
-  return (
-    <TabNav.Root justify="center">
-      <TabNav.Link asChild active={location === '/'} className={link}>
-        <Link href="/">Домой</Link>
-      </TabNav.Link>
-      <TabNav.Link asChild active={location === '/log'} className={link}>
-        <Link href="/log">Журнал</Link>
-      </TabNav.Link>
-      {/* <TabNav.Link asChild active={location === '/settings'} className={link}>
-        <Link href="/settings">Настройки</Link>
-      </TabNav.Link> */}
-    </TabNav.Root>
-  );
-};
+export const Navigation = () => (
+  <TabNav.Root justify="center">
+    {routes.map((route) => (
+      <NavLink route={route.route} key={route.route}>
+        {route.title}
+      </NavLink>
+    ))}
+  </TabNav.Root>
+);
